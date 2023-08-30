d33e7f4143fbfba12469b46ef9dce068183a6ae3
Can Ersoy <can.ersoy@tum.de>
2023-08-30 17:02:31 +0300

    matrix second set up

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class HomePage extends ConsumerWidget {
     return Scaffold(
       body: Center(
           child: Row(
-        children: [Matrix3(), Matrix3()],
+        children: [Matrix3_1(), Matrix3_2()],
       )),
     );
   }
@@ -56,9 +56,27 @@ StateProvider<TextEditingController> m1row3column2Provider =
     StateProvider((ref) => TextEditingController());
 StateProvider<TextEditingController> m1row3column3Provider =
     StateProvider((ref) => TextEditingController());
+StateProvider<TextEditingController> m2row1column1Provider =
+    StateProvider((ref) => TextEditingController());
+StateProvider<TextEditingController> m2row1column2Provider =
+    StateProvider((ref) => TextEditingController());
+StateProvider<TextEditingController> m2row1column3Provider =
+    StateProvider((ref) => TextEditingController());
+StateProvider<TextEditingController> m2row2column1Provider =
+    StateProvider((ref) => TextEditingController());
+StateProvider<TextEditingController> m2row2column2Provider =
+    StateProvider((ref) => TextEditingController());
+StateProvider<TextEditingController> m2row2column3Provider =
+    StateProvider((ref) => TextEditingController());
+StateProvider<TextEditingController> m2row3column1Provider =
+    StateProvider((ref) => TextEditingController());
+StateProvider<TextEditingController> m2row3column2Provider =
+    StateProvider((ref) => TextEditingController());
+StateProvider<TextEditingController> m2row3column3Provider =
+    StateProvider((ref) => TextEditingController());
 
-class Matrix3 extends ConsumerWidget {
-  const Matrix3({
+class Matrix3_1 extends ConsumerWidget {
+  const Matrix3_1({
     super.key,
   });
 
@@ -73,6 +91,120 @@ class Matrix3 extends ConsumerWidget {
     final row3col1 = ref.watch(m1row3column1Provider);
     final row3col2 = ref.watch(m1row3column2Provider);
     final row3col3 = ref.watch(m1row3column3Provider);
+    final _width = MediaQuery.of(context).size.width / 10;
+    final _height = MediaQuery.of(context).size.height / 5;
+
+    return Expanded(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                SizedBox(
+                  width: _width,
+                  height: _height,
+                  child: NumberInputWithIncrementDecrement(
+                    controller: row1col1,
+                  ),
+                ),
+                SizedBox(
+                  width: _width,
+                  height: _height,
+                  child: NumberInputWithIncrementDecrement(
+                    controller: row1col2,
+                  ),
+                ),
+                SizedBox(
+                  width: _width,
+                  height: _height,
+                  child: NumberInputWithIncrementDecrement(
+                    controller: row1col3,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                SizedBox(
+                  width: _width,
+                  height: _height,
+                  child: NumberInputWithIncrementDecrement(
+                    controller: row2col1,
+                  ),
+                ),
+                SizedBox(
+                  width: _width,
+                  height: _height,
+                  child: NumberInputWithIncrementDecrement(
+                    controller: row2col2,
+                  ),
+                ),
+                SizedBox(
+                  width: _width,
+                  height: _height,
+                  child: NumberInputWithIncrementDecrement(
+                    controller: row2col3,
+                  ),
+                )
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                SizedBox(
+                  width: _width,
+                  height: _height,
+                  child: NumberInputWithIncrementDecrement(
+                    controller: row3col1,
+                  ),
+                ),
+                SizedBox(
+                  width: _width,
+                  height: _height,
+                  child: NumberInputWithIncrementDecrement(
+                    controller: row3col2,
+                  ),
+                ),
+                SizedBox(
+                  width: _width,
+                  height: _height,
+                  child: NumberInputWithIncrementDecrement(
+                    controller: row3col3,
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class Matrix3_2 extends ConsumerWidget {
+  const Matrix3_2({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final row1col1 = ref.watch(m2row1column1Provider);
+    final row1col2 = ref.watch(m2row1column2Provider);
+    final row1col3 = ref.watch(m2row1column3Provider);
+    final row2col1 = ref.watch(m2row2column1Provider);
+    final row2col2 = ref.watch(m2row2column2Provider);
+    final row2col3 = ref.watch(m2row2column3Provider);
+    final row3col1 = ref.watch(m2row3column1Provider);
+    final row3col2 = ref.watch(m2row3column2Provider);
+    final row3col3 = ref.watch(m2row3column3Provider);
     final _width = MediaQuery.of(context).size.width / 10;
     final _height = MediaQuery.of(context).size.height / 5;
 
